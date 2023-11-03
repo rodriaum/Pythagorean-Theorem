6c0ba4a3bae8f989b4b650f8ccfaac851dd14adf
Rodrigo Ferreira <119365213+rodriaum@users.noreply.github.com>
2023-11-03 11:34:09 +0000

    Atualizar o PythagoreanTheorem.cpp (double)

--- PythagoreanTheorem.cpp
+++ PythagoreanTheorem.cpp
@@ -2,10 +2,9 @@
 
 int main()
 {
-
-    int hypotenuse;
-    int catetoA;
-    int catetoB;
+
+    double catetoA;
+    double catetoB;
 
     std::cout << "Digite o comprimento do primeiro cateto:\n";
     std::cin >> catetoA;
@@ -13,7 +12,7 @@ int main()
     std::cout << "Digite o comprimento do segundo cateto:\n";
     std::cin >> catetoB;
 
-    hypotenuse = sqrt((catetoA * catetoA) + (catetoB * catetoB));
+    double hypotenuse = sqrt((catetoA * catetoA) + (catetoB * catetoB));
 
     std::cout << "A hipotenusa é: " << hypotenuse;
 }
